bbdb14761a0dc91199dbbbee4642a8c0b447fbe8
UlysesCepedaC <A100228801@my.uvm.edu.mx>
2026-05-11 19:28:42 -0600

    Update agente_python.py

--- ejemplos/agente_python.py
+++ ejemplos/agente_python.py
@@ -22,6 +22,24 @@ class AgenteCiberseguridad:
         """
         return [str(r[0]).split("#")[-1] for r in self.grafo.query(consulta)]
 
+    def activo_afectado(self, amenaza):
+        consulta = f"""
+        SELECT ?activo WHERE {{
+        <{amenaza}> <{ns.afecta}> ?activo .
+        }}
+        """
+        return [str(r[0]).split("#")[-1] for r in self.grafo.query(consulta)]
+
+    def control_politica(self, politica):
+        consulta = f"""
+        SELECT ?control WHERE {{
+        <{politica}> <{ns.requiere}> ?control .
+        }}
+        """
+        return [str(r[0]).split("#")[-1] for r in self.grafo.query(consulta)]
+
 agente = AgenteCiberseguridad(g)
 
 print("Controles:", agente.recomendar(ns.MalwareRamsomware))
+print("Activo afectado:", agente.activo_afectado(ns.MalwareRamsomware))
+print("Control requerido:", agente.control_politica(ns.PoliticaSeguridadEmpresa))
